0fb0a28d8db593257c6b959a0d0f1663852f9e2d
Snail <snail@heeroik.com>
2015-08-18 21:30:55 -0400

    Nebula removed; basic window created

---
+++ src/starborn/version.hpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2015 Snailsoft <https://github.com/snailsoft/starborn/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define STARBORN_AUTHOR			"Snailsoft"
+#define STARBORN_DESCRIPTION	"Starborn"
+#define STARBORN_NAME			"Starborn"
+#define STARBORN_VERSION		"0.1.0"
+#define STARBORN_VERSION_NUMBER	0, 1, 0
