6b12f3c6553cdb608da4fe219ac76f8cc6efc62a
Snail <snail@heeroik.com>
2015-09-11 12:10:05 -0400

    A few textures have been updated; copyright updated

--- src/starborn/constants/animation_types.hpp
+++ src/starborn/constants/animation_types.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Snailsoft <https://github.com/snailsoft/starborn/>
+ * Copyright (C) 2013-2015 Snailsoft <https://github.com/snailsoft/starborn/>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
